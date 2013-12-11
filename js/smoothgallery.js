1acfc832185219fa76ac8a9fd939ce66483a5203
Kevin Thornbloom <kevin@kthornbloom.com>
2013-12-11 14:31:52 -0500

    Updating license

--- js/smoothgallery.js
+++ js/smoothgallery.js
@@ -1,3 +1,12 @@
+/*
+ * Smoothbox
+ * http://kthornbloom.com/smoothbox.php
+ *
+ * Copyright 2013, Kevin Thornbloom
+ * Free to use and abuse under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+ 
 (function ($) {
     $.fn.extend({
         // change 'pluginname' to your plugin name (duh)
